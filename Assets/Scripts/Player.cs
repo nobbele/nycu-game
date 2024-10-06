07e15b07d33af267ba7fc0e954ef44568aa64f04
nobbele <realnobbele@gmail.com>
2024-10-06 21:58:40 +0800

    Basic player stats/attribute and leveling up mechanic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerAttributes
+{
+    public int Damage = 0;
+    public int Speed = 0;
+    public int Stamina = 0;
+}
+public class Player : MonoBehaviour
+{
+
+    public int Experience;
+    public int Level;
+
+    public int Health = 10;
+
+    public int AttributePoints;
+
+    public PlayerAttributes Attributes;
+
+    public int XpRequired => 20 * Level + 100;
+
+    void Update()
+    {
+        if (Experience >= XpRequired)
+            LevelUp();
+
+        if (Health <= 0)
+            GameOver();
+
+        if (Input.GetKeyDown(KeyCode.U))
+            Experience += 10;
+    }
+
+    void LevelUp()
+    {
+        Experience = 0;
+        Level += 1;
+        AttributePoints += 1;
+        print($"Level Up: {Level - 1} -> {Level}");
+        print($"+1 Attribute Point");
+        // TODO Update UI
+    }
+
+    void GameOver()
+    {
+        print("Game over");
+        // TODO
+    }
+}
