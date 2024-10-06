73a7d5a990bf220ba8ecad4ef5ddca0116daa952
nobbele <realnobbele@gmail.com>
2024-10-06 20:49:27 +0800

    Implements mouse look

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,44 @@ public class Player : MonoBehaviour
 {
     public float Speed;
 
-    private new Rigidbody rigidbody;
+    public float Sensitivity;
 
-    void Start() {
+    private new Rigidbody rigidbody;
+    private new Camera camera;
+
+    private Vector2 currentRotation;
+    public float maxYAngle = 90f;
+
+    void Start()
+    {
         rigidbody = GetComponent<Rigidbody>();
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        // Handle cursor locking
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+
+        // We'll handle camera rotation only when the mouse locked for use.
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            currentRotation.x += Input.GetAxis("Mouse X") * Sensitivity;
+            currentRotation.y -= Input.GetAxis("Mouse Y") * Sensitivity;
+            currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+            currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+
+            /*
+             * For the horizontal rotation we rotate the player itself so they face the right direction.
+             * But for vertical rotation we'll only rotate the camera itself,
+             * so the player will still face forward and not tilt.
+             */
+            transform.rotation = Quaternion.Euler(0, currentRotation.x, 0);
+            camera.transform.localRotation = Quaternion.Euler(currentRotation.y, 0, 0);
+        }
     }
 
     void FixedUpdate()
